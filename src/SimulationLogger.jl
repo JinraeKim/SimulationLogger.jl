b19eda305111ef9c4b00b7a4e9f99daafcfdd493
JinraeKim <kjl950403@gmail.com>
2021-06-19 09:52:45 +0900

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/SimulationLogger.jl
@@ -0,0 +1,5 @@
+module SimulationLogger
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SimulationLogger
+using Test
+
+@testset "SimulationLogger.jl" begin
+    # Write your tests here.
+end
