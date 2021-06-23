f98a72be03d8dd2a129d2663243aa9e03201717f
Jinrae Kim <kjl950403@gmail.com>
2021-06-23 15:34:05 +0900

    Fix a bug (#4)

--- test/macros.jl
+++ test/macros.jl
@@ -3,19 +3,43 @@ using SimulationLogger
 
 function test()
     log = true
+    # log = false
+    test_log(log)
     test_nested_log(log)
     nothing
 end
 
+function test_log(log)
+    println("#"^10 * " test_log " * "#"^10)
+    y = 2
+    if log
+        __LOGGER_DICT__ = Dict()
+        @show @log y
+        @show @log x = y * 2
+        @show __LOGGER_DICT__
+    else
+        @show @log y
+        @show @log x = y * 2
+    end
+end
+
 function test_nested_log(log)
+    println("#"^10 * " test_nested_log " * "#"^10)
     y = 2
     if log
         __LOGGER_DICT__ = Dict()
         @show @nested_log :values x = 1
         # @show @nested_log :values x = 2  # new assignment with the same key will yield an error
         @show @nested_log :values y
+        @show @nested_log my_logging()
+        @show __LOGGER_DICT__
     else
         @show @nested_log :values x = 1
         @show @nested_log :values y
+        @show @nested_log my_logging()
     end
+end
+
+@Loggable function my_logging()
+    @nested_log :values z = 1
 end
